2e482263f4cc7c172bf5bd59277da27a0d3072a4
ekicyou <dot.station@gmail.com>
2019-06-29 22:14:14 +0900

    build batの作成

--- ghost/build-emo.bat
+++ ghost/build-emo.bat
@@ -3,5 +3,6 @@ pushd %~dp0
   pushd ..\..\emo-rs
     cargo build --release --target i686-pc-windows-msvc
     popd
+    copy ..\..\emo-rs\target\i686-pc-windows-msvc\release\emo.dll emo-gs\ghost\master\
   
   popd
